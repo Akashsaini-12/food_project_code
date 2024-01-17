b8a28498fc17381ffb6338a281e743553f3a3d17
akashsaini <akashsaini5377@gmail.com>
2024-01-17 22:51:02 +0530

    first commit code this

--- App.js
+++ App.js
@@ -1,27 +1,15 @@
-// import * as React from 'react';
-// import {View, Text} from 'react-native';
-// import Navigation from './Component/Navigation/Navigation'
-// import {NavigationContainer} from '@react-navigation/native';
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import Navigation from './Component/Navigation/Navigation'
+import {NavigationContainer} from '@react-navigation/native';
 
-// function App() {
-//   return (
-//     <NavigationContainer>
-//       <Navigation></Navigation>
-//     </NavigationContainer>
-//   );
-// }
-
-// export default App;
-
-import { View, Text } from 'react-native'
-import React from 'react'
-
-const App = () => {
+function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
+    <NavigationContainer>
+      <Navigation></Navigation>
+    </NavigationContainer>
+  );
 }
 
-export default App
+export default App;
+
